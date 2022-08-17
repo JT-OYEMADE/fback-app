b44fbb9ffbbc196f56ca4614756cbc8898c2b6d1
J.T. Oyemade <admin@Tomis-MacBook-Pro.local>
2022-08-17 13:01:53 +0100

    initial

---
+++ src/data/FeedbackData.js
@@ -0,0 +1,20 @@
+const FeedbackData = 
+[
+  {
+    id: 1,
+    rating: 10,
+    text: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dicta illo facere alias laborum corrupti quas molestias distinctio sequi doloremque expedita?",
+  },
+  {
+    id: 2,
+    rating: 9,
+    text: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dicta illo facere alias laborum corrupti quas molestias distinctio sequi doloremque expedita?",
+  },
+  {
+    id: 3,
+    rating: 8,
+    text: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dicta illo facere alias laborum corrupti quas molestias distinctio sequi doloremque expedita?",
+  }
+]
+
+export default FeedbackData
